0dbdced9c04c9bd430edf8bb4175a0a68593653d
Sabry Awad <dr.sabry97@gmail.com>
2024-10-11 14:20:48 +0300

    🔧 refactor(src-tauri): Optimize development and release profiles 🔧 refactor(src-tauri): Update window configuration 🔄 chore(src-tauri): Update icon paths in bundle configuration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,14 @@
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
@@ -11,6 +18,7 @@ pub fn run() {
             }
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
